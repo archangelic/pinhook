074dc8386136010eb4203f6a988d1222afea4f6d
Mallory Hancock <malloryhancock89@gmail.com>
2017-10-02 11:01:51 -0700

    Update to version 0.1.0

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 
 setup(
     name='pinhook',
+    version='0.1.0',
+    license="MIT",
+    description="A pluggable IRC bot framework in Python",
     install_requires=[
         'irc',
     ],
