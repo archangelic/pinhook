cc17b9ae7244e3ec2845490372b59d77baf854f8
Mallory Hancock <malloryhancock89@gmail.com>
2017-10-10 08:56:21 -0700

    update to new object argument

--- examples/plugins/test.py
+++ examples/plugins/test.py
@@ -1,7 +1,6 @@
 import pinhook.plugin
 
 @pinhook.plugin.register('!test')
-def test(**kwargs):
-    nick = kwargs['nick']
-    return pinhook.plugin.message("{}: Test".format(nick))
+def test(msg):
+    return pinhook.plugin.message("{}: Test".format(msg.nick))
 
