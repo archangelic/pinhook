27aa7a2e06dc0a6942c32cbc26d9404090ee2fd8
Mallory Hancock <malloryhancock89@gmail.com>
2017-10-10 08:57:15 -0700

    increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pinhook',
-    version='0.1.1',
+    version='0.2.0',
     license="MIT",
     description="A pluggable IRC bot framework in Python",
     install_requires=[
