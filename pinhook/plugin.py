172c7b439cf6609e1d8531bdf140c346cd439f3b
importantchoice <22687029+importantchoice@users.noreply.github.com>
2017-11-30 10:08:31 +0100

    Added register_regex decorator
    The register_regex decorator lets the user define a custom regular
    expression. If this regex matches against any incoming message, the plugin
    function is called.

--- pinhook/plugin.py
+++ pinhook/plugin.py
@@ -1,4 +1,5 @@
 cmds = []
+regexes = []
 
 class Output:
     def __init__(self, msg_type, msg):
@@ -21,9 +22,19 @@ def add_plugin(command, func):
     cmds.append({'cmd': command, 'func': func})
 
 
+def add_regex(regex, func):
+    regexes.append({'regex': regex, 'func': func})
+
+
 def register(command):
     def register_for_command(func):
         add_plugin(command, func)
         return func
     return register_for_command
 
+
+def register_regex(regex):
+    def register_for_regex(func):
+        add_regex(regex, func)
+        return func
+    return register_for_regex
