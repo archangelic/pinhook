97316c750b6017ac498c5ebaa558fdb75171c177
Mallory Hancock <malloryhancock89@gmail.com>
2018-01-12 11:18:38 -0800

    iterate version for fix

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ class UploadCommand(Command):
 # Where the magic happens:
 setup(
     name=NAME,
-    version='1.3.3',
+    version='1.3.4',
     description=DESCRIPTION,
     long_description=long_description,
     author=AUTHOR,
