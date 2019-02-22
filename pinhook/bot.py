5e46ab940df4abe00257b32341d3a68117044454
Mallory Hancock <malloryhancock89@gmail.com>
2019-02-22 12:56:21 -0800

    add some debugging statements

--- pinhook/bot.py
+++ pinhook/bot.py
@@ -163,6 +163,7 @@ class Bot(irc.bot.SingleServerIRCBot):
                     if pinhook.plugin.cmds[cmd]['ops_msg']:
                         output =  self.output_message(pinhook.plugin.cmds[cmd]['ops_msg'])
                 else:
+                    logger.debug('executing {}'.format(cmd))
                     output = pinhook.plugin.cmds[cmd]['run'](self.Message(
                         channel=chan,
                         cmd=cmd,
@@ -176,11 +177,13 @@ class Bot(irc.bot.SingleServerIRCBot):
                         ops=self.ops,
                         logger=self.logger
                     ))
+                    logger.debug(output)
             except Exception as e:
                 self.logger.exception('issue with command {}'.format(cmd))
         else:
             for lstnr in pinhook.plugin.lstnrs:
                 try:
+                    logger.debug('whispering to listener: {}'.format(lstnr))
                     output = pinhook.plugin.lstnrs[lstnr](self.Message(
                         channel=chan,
                         text=text,
@@ -193,6 +196,7 @@ class Bot(irc.bot.SingleServerIRCBot):
                         ops=self.ops,
                         logger=self.logger
                     ))
+                    logger.debug(output)
                 except Exception as e:
                     self.logger.exception('issue with listener {}'.format(lstnr))
         return output
